385251ccf3101d1d9a735e3399aa3e08cdd3286e
Ray Sweeten <rsweeten@amplience.com>
2023-07-25 10:45:19 -0400

    feat: surface all active campaign/promo IDs

--- cartridges/int_ampliencebridge/cartridge/scripts/hooks/site.js
+++ cartridges/int_ampliencebridge/cartridge/scripts/hooks/site.js
@@ -1,5 +1,7 @@
 exports.modifyGETResponse = function (scriptFolder, results) {
   const Status = require("dw/system/Status");
+  
+  // Get Customer Groups
   const CustomerMgr = require("dw/customer/CustomerMgr");
   let customerGroups = [];
 
@@ -11,6 +13,26 @@ exports.modifyGETResponse = function (scriptFolder, results) {
   }
 
   results.c_customerGroups = customerGroups;
+
+  // Get all active campaigns and promotions
+  const CM = require('dw/campaign/PromotionMgr');
+  let camps = [];
+  let promotions = [];
+  var cams = CM.getCampaigns().iterator();
+  var proms = CM.getPromotions().iterator();
+
+  while(cams.hasNext()){
+      let cam = cams.next()
+      if(cam.active === true) {camps.push(cam.ID)};
+  }
+
+  while(proms.hasNext()){
+      let prom = proms.next()
+      if(prom.active === true) {promotions.push(prom.ID)};
+  }
+
+  results.c_activeCampaignIds = camps;
+  results.c_activePromoIds = promotions;
 
   return new Status(Status.OK);
 };
